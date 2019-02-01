20b2c7ba1e63452a09ba3406263ec53ef2aeb9a3
Vitor Prata <vitorbretasprata@gmail.com>
2019-02-01 21:01:46 -0200

    i

--- src/server.js
+++ src/server.js
@@ -22,24 +22,7 @@ const port = process.env.PORT || 2000;
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(cors(corsOptions));
-server.use((req, res, next) => {
-    var token = req.headers['authorization'];
-    if(!token) return next();
 
-    token = token.replace('Bearer ', '');
-
-    jwt.verify(token, config.secret, (err, user) => {
-        if(err){
-            return res.status(401).send({
-                success: false,
-                message: 'Please register Log in using a valid email'
-            });
-        } else {
-            req.user = user;
-            next();
-        }
-    })
-});
 
 server.use('/api/auth', auth);
 server.use('/api/tickets', tickets);
