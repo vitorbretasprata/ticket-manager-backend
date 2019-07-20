7ff07cd11c7b39a24695f320a8830e9b1f8fa90f
Vitor Prata <vitorbretasprata@gmail.com>
2019-07-20 16:25:01 -0300

    a

--- src/routes/tickets.js
+++ src/routes/tickets.js
@@ -10,4 +10,5 @@ routes.post('/filter' , validateJWT, controller.Filter);
 routes.delete('/deleteTicket/:id' , validateJWT, controller.deleteTicket);
 routes.put('/editTicket/:id' , validateJWT, controller.editTicket);
 
+
 module.exports = routes
